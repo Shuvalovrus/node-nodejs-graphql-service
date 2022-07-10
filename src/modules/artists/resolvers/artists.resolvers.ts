4fb2c59b3953786422cf24ef9d97f9d55a20ffe3
Konstantin <shuvalov.2021@list.ru>
2022-07-10 14:27:27 +0300

    feat: update resolvers for artists

--- src/modules/artists/resolvers/artists.resolvers.ts
+++ src/modules/artists/resolvers/artists.resolvers.ts
@@ -3,4 +3,9 @@ module.exports = {
     getArtist: (_, { id }, { dataSources }) => dataSources.artists.getArtist(id),
     getArtists: (_, __, { dataSources }) => dataSources.artists.getArtists(),
   },
+  Artist: {
+    bands: ({ bandsIds }, __, { dataSources }) => bandsIds.map((id) => {
+      return dataSources.bands.getBands(id);
+    }),
+  },
 };
