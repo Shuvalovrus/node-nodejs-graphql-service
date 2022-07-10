0f1b470f5533e7282a5e9d745caeaccc80eb3fa0
Konstantin <shuvalov.2021@list.ru>
2022-07-10 20:01:04 +0300

    feat: add Mutations for genres

--- src/modules/genres/resolvers/genres.resolvers.ts
+++ src/modules/genres/resolvers/genres.resolvers.ts
@@ -1,6 +1,11 @@
 module.exports = {
   Query: {
-    getGenre: (_, args, { dataSources }) => dataSources.genres.getGenre(args),
+    getGenre: (_, { id }, { dataSources }) => dataSources.genres.getGenre(id),
     getGenres: (_, __, { dataSources }) => dataSources.genres.getGenres(),
+  },
+  Mutation: {
+    createGenre: (_, data, { dataSources }) => dataSources.genres.createGenre(data),
+    deleteGenre: (_, { id }, { dataSources }) => dataSources.genres.deleteGenre(id),
+    updateGenre: (_, data, { dataSources }) => dataSources.genres.updateGenre(data),
   },
 };
--- src/modules/genres/services/genres-services.ts
+++ src/modules/genres/services/genres-services.ts
@@ -8,15 +8,30 @@ class GenresServices extends RESTDataSource {
     this.baseURL = process.env.GENRES_URL;
   }
 
+  willSendRequest(request) {
+    request.headers.set('Authorization', this.context.token);
+  }
+
   async getGenre(id) {
-    const data = await this.get(`/${id.id}`);
-    // eslint-disable-next-line no-underscore-dangle
-    return { id: data._id, ...data };
+    return this.get(`/${id}`);
   }
 
   async getGenres() {
     const data = await this.get('');
     return data.items;
+  }
+
+  async createGenre(data) {
+    return this.post('', data.data);
+  }
+
+  async deleteGenre(id) {
+    return this.delete(`/${id}`);
+  }
+
+  async updateGenre(data) {
+    // eslint-disable-next-line no-underscore-dangle
+    return this.put(`/${data.data._id}`, { ...data.data });
   }
 }
 
