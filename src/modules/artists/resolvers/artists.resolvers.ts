0b9ea94278e8b10e45ffcaf77b7756bdc8c99377
Konstantin <shuvalov.2021@list.ru>
2022-07-10 20:14:41 +0300

    refactor: fix eslint errors

--- src/modules/artists/resolvers/artists.resolvers.ts
+++ src/modules/artists/resolvers/artists.resolvers.ts
@@ -4,19 +4,11 @@ module.exports = {
     getArtists: (_, __, { dataSources }) => dataSources.artists.getArtists(),
   },
   Mutation: {
-    createArtist: (_, data, { dataSources }) => {
-      return dataSources.artists.createArtist(data);
-    },
-    deleteArtist: (_, { id }, { dataSources }) => {
-      return dataSources.artists.deleteArtist(id);
-    },
-    updateArtist: (_, data, { dataSources }) => {
-      return dataSources.artists.updateArtist(data);
-    },
+    createArtist: (_, data, { dataSources }) => dataSources.artists.createArtist(data),
+    deleteArtist: (_, { id }, { dataSources }) => dataSources.artists.deleteArtist(id),
+    updateArtist: (_, data, { dataSources }) => dataSources.artists.updateArtist(data),
   },
   Artist: {
-    bands: ({ bandsIds }, __, { dataSources }) => bandsIds.map((id) => {
-      return dataSources.bands.getBand(id);
-    }),
+    bands: ({ bandsIds }, __, { dataSources }) => bandsIds.map((id) => dataSources.bands.getBand(id)),
   },
 };
