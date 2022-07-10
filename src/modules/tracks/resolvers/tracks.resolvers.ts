176744cc22269e78e7352b8dd67d18c787e011fd
Konstantin <shuvalov.2021@list.ru>
2022-07-10 14:32:17 +0300

    feat: update resolvers for Track

--- src/modules/tracks/resolvers/tracks.resolvers.ts
+++ src/modules/tracks/resolvers/tracks.resolvers.ts
@@ -3,4 +3,16 @@ module.exports = {
     getTrack: (_, { id }, { dataSources }) => dataSources.tracks.getTrack(id),
     getTracks: (_, __, { dataSources }) => dataSources.tracks.getTracks(),
   },
+
+  Track: {
+    artists: ({ artistsIds }, __, { dataSources }) => artistsIds.map((id) => {
+      return dataSources.artists.getArtist(id);
+    }),
+    bands: ({ bandsIds }, __, { dataSources }) => bandsIds.map((id) => {
+      return dataSources.bands.getBands(id);
+    }),
+    genres: ({ genresIds }, __, { dataSources }) => genresIds.map((id) => {
+      return dataSources.genres.getGenre(id);
+    }),
+  },
 };
